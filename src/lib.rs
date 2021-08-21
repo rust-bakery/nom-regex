a4c4801019a22adc1537b8a3a1cc902e16f0e182
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-08-21 13:01:59 +0200

    some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,20 @@
 //! Parser combinators that use regular expressions.
+//!
+//! This crate provides combinators for [nom parser combinators](https://crates.io/crates/nom) using the [regex](https://crates.io/crates/regex) crate.
+//!
+//! ## Example
+//!
+//! ```rust,no_run
+//! use nom::{Err, error::ErrorKind};
+//! use nom_regex::str::re_match;
+//! fn main() {
+//!   let re = regex::Regex::new(r"^\d{4}").unwrap();
+//!   let parser = re_match::<(&str, ErrorKind)>(re);
+//!   assert_eq!(parser("2019"), Ok(("", "2019")));
+//!   assert_eq!(parser("abc"), Err(Err::Error(("abc", ErrorKind::RegexpMatch))));
+//!   assert_eq!(parser("2019-10"), Ok(("", "2019-10")));
+//! }
+//! ```
 #[macro_use]
 extern crate nom;
 
